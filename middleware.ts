9d97236e3cacfabbc9360ebb5eda9fe257acdd9e
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-06-07 20:43:44 +0200

    Ajout des logs de middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,11 @@ import { route } from '@/lib/route';
 export default authMiddleware({
   publicRoutes: ['/', '/api/webhook'],
   afterAuth(auth, req) {
+    console.log({ auth, req });
     if (auth.userId && auth.isPublicRoute) {
+      console.log({
+        'auth.userId && auth.isPublicRoute': auth.userId && auth.isPublicRoute,
+      });
       let path = '/select-org';
       if (auth.orgId) {
         path = route('/organization/[organizationId]', {
@@ -19,10 +23,18 @@ export default authMiddleware({
     }
 
     if (!auth.userId && !auth.isPublicRoute) {
+      console.log({
+        '!auth.userId && !auth.isPublicRoute':
+          !auth.userId && !auth.isPublicRoute,
+      });
       return redirectToSignIn({ returnBackUrl: req.url });
     }
 
     if (auth.userId && !auth.orgId && req.nextUrl.pathname !== '/select-org') {
+      console.log({
+        "auth.userId && !auth.orgId && req.nextUrl.pathname !== '/select-org'":
+          auth.userId && !auth.orgId && req.nextUrl.pathname !== '/select-org',
+      });
       const orgSelection = new URL('/select-org', req.url);
       return NextResponse.redirect(orgSelection);
     }
